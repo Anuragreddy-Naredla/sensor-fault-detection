8fa27ee360d7acc073d2cf789872640281d5f0c8
Naredla Anurag Reddy <n.anurag@beta-cae.in>
2022-11-11 18:23:21 +0530

    codes

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    This function will return list of requirements
+
+    Returns:
+        List[str]: _description_
+    """
+    requirement_list:List[str] = []
+    """
+    write a code to read requirements.txt file and append each requirements in requirements_list variable.
+    """
+
+    return requirement_list
+
+setup(
+
+    name = "sensor",
+    version = "0.0.1",
+    author = "Anurag Reddy Naredla",
+    author_email = "anuragreddy1241@gmail.com",
+    # It will search for all packages.
+    packages = find_packages(),
+    install_requires = get_requirements()#["pymongo==4.2.0"],
+
+)
